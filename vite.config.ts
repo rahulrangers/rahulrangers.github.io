9f5bbc2ab3fbbb0990301f730c2b19fad095a2e3
rahulrangers <127782777+rahulrangers@users.noreply.github.com>
2024-01-11 02:27:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "rahulrangers.github.io"
+  base: "rahulrangers.github.io", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
