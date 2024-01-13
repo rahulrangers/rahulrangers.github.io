4b0068ef208f7c01aeaef9e41822620256ba5b5b
unknown <rahulreddypurmani123@gmail.com>
2024-01-14 01:10:26 +0530

    updated Home.tsx

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,5 +1,4 @@
 import image from "../assets/background.jpg";
-import resume from "../assets/Resume.pdf"
 import { Button } from "./ui/button"
 const Home = () => {
   return (
@@ -13,15 +12,7 @@ const Home = () => {
      <div className="font-bold flex flex-wrap md:w-[400px] lg:w-full ">I AM A SECOND YEAR UNDERGRAD AT COMPUTER SCIENCE DEPARTMENT, IIT JODHPUR.</div>
      <div className="font-bold">LOVE TO LEARN AND EXPLORE NEW TECHNOLOGIES</div>
      <div className="py-10 ">
-     <Button onClick={()=>{
-      const url = window.URL.createObjectURL(new Blob([resume]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download','resume')
-      document.body.appendChild(link)
-      link.click();
-      document.body.removeChild(link)
-     }}>DOWNLOAD RESUME</Button>
+     <Button>DOWNLOAD RESUME</Button>
      </div>
      </div>
     </div>
